e478966bf2e1201f15ff9ca1f605ce4d716d73a1
Anatoly Fedyanin <anfemail@gmail.com>
2024-02-16 13:36:49 +0300

    add tests

--- src/Vertr.Moex.Iss/Entities/InfoBlock.cs
+++ src/Vertr.Moex.Iss/Entities/InfoBlock.cs
@@ -14,19 +14,48 @@ public class InfoBlock
 
     public IReadOnlyDictionary<string, MetadataItem> Metadata { get; private set; }
 
-    public InfoBlock(string key, JsonDocument jsonDocument)
+    public InfoBlock(string key, string json, bool fillData = false) :
+        this(key, JsonDocument.Parse(json), fillData)
     {
-        if (!jsonDocument.RootElement.TryGetProperty(key, out var jsonElement))
+    }
+
+    public InfoBlock(string key, JsonDocument jsonDocument, bool fillData = false) :
+        this(key, GetPropertyOrThrow(jsonDocument.RootElement, key), fillData)
+    {
+    }
+
+    public InfoBlock(string key, JsonElement jsonElement, bool fillData = false)
+    {
+        _jsonElement = jsonElement;
+        Key = key;
+        Columns = GetPropertyOrThrow(_jsonElement, "columns").Deserialize<string[]>() ?? [];
+        Metadata = GetPropertyOrThrow(_jsonElement, "metadata").Deserialize<Dictionary<string, MetadataItem>>() ?? [];
+        Data = new DataFrame(Columns.Select(CreateColumn));
+
+        if (fillData)
         {
-            throw new ArgumentException($"key={key} is not found in {nameof(JsonDocument)}");
+            FillDataFrame(GetPropertyOrThrow(_jsonElement, "data"));
+        }
+    }
+
+    public void FillDataFrame(JsonElement jsonDataElement)
+    {
+        foreach (var jsonRow in jsonDataElement.EnumerateArray())
+        {
+            Data.Append(CreateDataRow(jsonRow), true);
+        }
+    }
+
+    private List<object?> CreateDataRow(JsonElement jsonRow)
+    {
+        var res = new List<object?>();
+
+        for (int i = 0; i < Columns.Length; i++)
+        {
+            res.Add(ConvertToCell(jsonRow[i], Metadata[Columns[i]].Type));
         }
 
-        _jsonElement = jsonElement;
-
-        Key = key;
-        Columns = _jsonElement.GetProperty("columns").Deserialize<string[]>() ?? [];
-        Metadata = _jsonElement.GetProperty("metadata").Deserialize<Dictionary<string, MetadataItem>>() ?? [];
-        Data = new DataFrame(Columns.Select(CreateColumn));
+        return res;
     }
 
     private DataFrameColumn CreateColumn(string colName)
@@ -48,5 +77,70 @@ public class InfoBlock
             "datetime" => new PrimitiveDataFrameColumn<DateTime>(colName),
             _ => throw new InvalidOperationException($"Unsupported column type: {metaItem.Type}")
         };
+    }
+
+    private static object? ConvertToCell(JsonElement jsonCell, string columnType)
+    {
+        if (jsonCell.ValueKind is
+            JsonValueKind.Null or
+            JsonValueKind.Undefined or
+            JsonValueKind.Array) // unsupported
+        {
+            return null;
+        }
+
+        return columnType switch
+        {
+            "string" => jsonCell.GetString() ?? string.Empty,
+            "double" => jsonCell.GetDecimal(),
+            "int32" => jsonCell.GetInt32(),
+            "int64" => jsonCell.GetInt64(),
+            "date" => GetDateOnly(jsonCell.GetString()),
+            "time" => GetTimeOnly(jsonCell.GetString()),
+            "datetime" => GetDateTime(jsonCell.GetString()),
+            "undefined" => null,
+            "unknown" => null,
+            _ => throw new ArgumentException($"Unsupported column type: {columnType}"),
+        };
+    }
+
+    private static DateOnly? GetDateOnly(string? value)
+    {
+        if (!DateOnly.TryParse(value, out var res))
+        {
+            return null;
+        }
+
+        return res;
+    }
+
+    private static TimeOnly? GetTimeOnly(string? value)
+    {
+        if (!TimeOnly.TryParse(value, out var res))
+        {
+            return null;
+        }
+
+        return res;
+    }
+
+    private static DateTime? GetDateTime(string? value)
+    {
+        if (!DateTime.TryParse(value, out var res))
+        {
+            return null;
+        }
+
+        return res;
+    }
+
+    private static JsonElement GetPropertyOrThrow(JsonElement jsonElement, string name)
+    {
+        if (!jsonElement.TryGetProperty(name, out var found))
+        {
+            throw new ArgumentException($"Json property={name} is not found.");
+        }
+
+        return found;
     }
 }
--- tests/Vertr.Moex.Iss.Tests/Entities/InfoBlockTests.cs
+++ tests/Vertr.Moex.Iss.Tests/Entities/InfoBlockTests.cs
@@ -6,25 +6,67 @@ namespace Vertr.Moex.Iss.Tests.Entities;
 [TestFixture(Category = "Integration")]
 public class InfoBlockTests
 {
-    [TestCase("JsonData/bonds.json")]
-    [TestCase("JsonData/shares.json")]
-    [TestCase("JsonData/bonds.columns.json")]
+    [TestCase("JsonData/bonds.json", "securities")]
+    [TestCase("JsonData/shares.json", "securities")]
+    [TestCase("JsonData/bonds.columns.json", "securities")]
+    [TestCase("JsonData/shares.columns.json", "securities")]
+    [TestCase("JsonData/bonds.json", "marketdata")]
+    [TestCase("JsonData/shares.json", "marketdata")]
+    [TestCase("JsonData/bonds.columns.json", "marketdata")]
+    [TestCase("JsonData/shares.columns.json", "marketdata")]
+    [TestCase("JsonData/bonds.json", "marketdata_yields")]
+    [TestCase("JsonData/shares.json", "marketdata_yields")]
+    [TestCase("JsonData/bonds.columns.json", "marketdata_yields")]
+    [TestCase("JsonData/shares.columns.json", "marketdata_yields")]
+    public void CanCreateInfoBlockWithEmptyData(string fileName, string blockName)
+    {
+        var json = File.ReadAllText(fileName);
+
+        var securitiesBlock = new InfoBlock(blockName, json);
+        Assert.That(securitiesBlock, Is.Not.Null);
+        Assert.That(securitiesBlock.Data.Columns, Is.Not.Empty);
+    }
+
+    [TestCase("JsonData/bonds.json", "securities")]
+    [TestCase("JsonData/shares.json", "securities")]
+    [TestCase("JsonData/bonds.columns.json", "securities")]
+    [TestCase("JsonData/shares.columns.json", "securities")]
+    [TestCase("JsonData/bonds.json", "marketdata")]
+    [TestCase("JsonData/shares.json", "marketdata")]
+    [TestCase("JsonData/bonds.columns.json", "marketdata")]
+    [TestCase("JsonData/shares.columns.json", "marketdata")]
+    [TestCase("JsonData/bonds.json", "marketdata_yields")]
+    [TestCase("JsonData/shares.json", "marketdata_yields")]
+    [TestCase("JsonData/bonds.columns.json", "marketdata_yields")]
+    [TestCase("JsonData/shares.columns.json", "marketdata_yields")]
+    public void CanFillDataFrameWithData(string fileName, string blockName)
+    {
+        var json = File.ReadAllText(fileName);
+
+        var block = new InfoBlock(blockName, json, true);
+
+        Assert.That(block, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(block.Data.Columns, Is.Not.Empty);
+            Assert.That(block.Data.Rows, Is.Not.Empty);
+        });
+    }
+
     [TestCase("JsonData/shares.columns.json")]
-    public void CanCreateInfoBlockWithEmptyData(string fileName)
+    public void CanFillDataRows(string fileName)
     {
         var json = File.ReadAllText(fileName);
         var jDoc = JsonDocument.Parse(json);
 
         var securitiesBlock = new InfoBlock("securities", jDoc);
-        Assert.That(securitiesBlock, Is.Not.Null);
-        Assert.That(securitiesBlock.Data.Columns, Is.Not.Empty);
+        var jsonData = jDoc.RootElement.GetProperty("securities").GetProperty("data");
 
-        var marketDataBlock = new InfoBlock("marketdata", jDoc);
-        Assert.That(marketDataBlock, Is.Not.Null);
-        Assert.That(marketDataBlock.Data.Columns, Is.Not.Empty);
+        securitiesBlock.FillDataFrame(jsonData);
+        var df = securitiesBlock.Data;
 
-        var marketDataYieldsBlock = new InfoBlock("marketdata_yields", jDoc);
-        Assert.That(marketDataYieldsBlock, Is.Not.Null);
-        Assert.That(marketDataYieldsBlock.Data.Columns, Is.Not.Empty);
+        var table = df.ToString();
+        Assert.That(table, Is.Not.Empty);
+        Console.WriteLine(table);
     }
 }
